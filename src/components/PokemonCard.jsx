5e8e65b3badef2f2e6c20c36c4d8581d3d667883
Paulina <paulinavelasqv@gmail.com>
2023-12-29 22:13:49 +1300

    Se agrega tarjeta flotante al hacer click sobre un pokemon

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,11 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 
-const PokemonCard = ({ name, id, image }) => (
-  <div className="pokemon-card border p-4 rounded-md shadow-md h-96 flex flex-col items-center bg-gray-100">
-    <img src={image} alt={name} className="mx-auto mb-2 mt-2 h-64 w-64 object-contain" />
-    <p className="text-center font-semibold text-lg">{name}</p>
-    <p className="text-center font-semibold text-lg">#{id}</p>
-  </div>
-);
+const PokemonCard = ({ name, id, image }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleBackgroundClick = (e) => {
+    // Cerrar la tarjeta flotante solo si se hace clic en el fondo oscuro
+    if (e.target.classList.contains("bg-opacity-50")) {
+      closeModal();
+    }
+  };
+
+  return (
+    <div className="pokemon-card border p-4 rounded-md shadow-md h-96 flex flex-col items-center bg-gray-100 relative">
+      <img
+        src={image}
+        alt={name}
+        className="mx-auto mb-2 mt-2 h-64 w-64 object-contain cursor-pointer"
+        onClick={openModal}
+      />
+      <p className="text-center font-semibold text-lg">{name}</p>
+      <p className="text-center font-semibold text-lg">#{id}</p>
+
+      {isModalOpen && (
+        <div
+          className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-50"
+          onClick={handleBackgroundClick}
+        >
+          <div className="bg-white p-4 rounded-md relative">
+            <button
+              className="absolute top-2 right-2 text-xl cursor-pointer"
+              onClick={closeModal}
+            >
+              X
+            </button>
+            <p>ID: {id}</p>
+            <p>Name: {name}</p>
+            {/* Otros detalles del Pokémon */}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default PokemonCard;
