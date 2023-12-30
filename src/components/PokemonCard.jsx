4fefc147701b5c2a20c3be6e593093c471f7804f
Paulina <paulinavelasqv@gmail.com>
2023-12-30 17:37:24 +1300

    Se continua modificando la tarjeta flotante

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -68,6 +68,8 @@ const PokemonCard = ({ data, flag }) => {
             {" "}
             {/* Añade la clase relative */}
             <PokemonCardExpanded
+              image={flag ? `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${
+                selectedPokemon.url.split("/")[6]}.png` : selectedPokemon.sprites.other.dream_world.front_default}
               name={selectedPokemon.name}
               id={flag ? selectedPokemon.url.split("/")[6] : selectedPokemon.id}
             />
--- src/components/PokemonCardExpanded.jsx
+++ src/components/PokemonCardExpanded.jsx
@@ -1,10 +1,22 @@
-import React from "react";
+const PokemonCardExpanded = ({ image, name, id }) => (
+  <div className="bg-white p-0 rounded-md overflow-hidden relative">
+    {/* Primera tarjeta con imagen y nombre */}
+    <div className="flex items-center">
+      <img src={image} alt={name} className="h-52 w-96 object-contain" />
+      <div className="ml-auto text-right p-6">
+        <p className="text-2xl font-semibold">{name}</p>
+      </div>
+    </div>
 
-const PokemonCardExpanded = ({ name, id }) => (
-  <div className="bg-white p-4 rounded-md">
-    <p>ID: {id}</p>
-    <p>Name: {name}</p>
-    {/* Otros detalles del Pokémon */}
+    {/* Segunda tarjeta con datos del Pokémon */}
+    <div className="bg-blue-500 text-white p-4">
+      <p className="text-lg">ID: {id}</p>
+      <p className="text-lg">ID: {id}</p>
+      <p className="text-lg">ID: {id}</p>
+      <p className="text-lg">ID: {id}</p>
+      <p className="text-lg">ID: {id}</p>
+      {/* Otros datos del Pokémon */}
+    </div>
   </div>
 );
 
